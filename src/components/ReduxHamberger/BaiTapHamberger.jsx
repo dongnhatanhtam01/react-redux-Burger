7ce7a10a708ebd1106db6b3d48ef8ea1a7eb70f4
tamdna <tamdna@vietmap.vn>
2020-12-15 00:56:13 +0700

    done hamburger +-

--- src/components/ReduxHamberger/BaiTapHamberger.jsx
+++ src/components/ReduxHamberger/BaiTapHamberger.jsx
@@ -3,19 +3,36 @@ import { connect } from "react-redux";
 // css
 import "./hamberger.css";
 class BaiTapHamberger extends Component {
+  renderMenuHandler = () => {
+    let { menu, burger } = this.props;
+    return Object.entries(menu).map(([varKey, varValue], index) => {
+      return (
+        <tr key={index}>
+          <td>{varKey}</td>
+          <td>
+            <button className="btn btn-danger m-2" onClick={()=>this.props.addBreadMid(varKey, -1)}>-</button>
+            {burger[varKey]}
+            <button className="btn btn-success m-2" onClick={()=>this.props.addBreadMid(varKey, 1)}>+</button>
+          </td>
+          <td>{varValue}</td>
+          <td>{burger[varKey] * varValue}</td>
+        </tr>
+      );
+    });
+  };
   renderBreadMidHandler = () => {
     // console.log(this.props.burger);
     // ky thuat duyet object
     let { burger } = this.props;
     let content = [];
     for (let propsBurger in burger) {
-        let breadMid = [];
-        for (let i=0; i< burger[propsBurger]; i++){
-            breadMid.push(<div className={propsBurger} key={i}></div>)
-        }
-        content.push(breadMid)
+      let breadMid = [];
+      for (let i = 0; i < burger[propsBurger]; i++) {
+        breadMid.push(<div className={propsBurger} key={i}></div>);
+      }
+      content.push(breadMid);
     }
-    return content
+    return content;
     // return Object.entries(burger).map(([var1_key, var2_value], index) =>{
     //     console.log(var1_key, var2_value);
 
@@ -41,17 +58,46 @@ class BaiTapHamberger extends Component {
             {this.renderBreadMidHandler()}
             <div className="breadBottom"></div>
           </div>
-          <div className="col-6">12</div>
+          <div className="col-6">
+            <table className="table">
+              <thead>
+                <td>Thức ăn</td>
+                <td></td>
+                <td>Giá</td>
+                <td>Thành Tiền:</td>
+              </thead>
+              <tbody>{this.renderMenuHandler()}</tbody>
+              <tfoot>
+                <td colSpan="2"></td>
+                <td>Tổng tiền</td>
+                <td>{this.props.total}</td>
+              </tfoot>
+            </table>
+          </div>
         </div>
       </div>
     );
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBreadMid: (propsBurger, amount) => {
+      const action = {
+        type: "ADD_BREAD_MID",
+        propsBurger,
+        amount,
+      };
+      dispatch(action);
+    },
+  };
+};
 const mapStateToProps = (state) => {
   return {
     burger: state.BurgerReducer.burger,
+    menu: state.BurgerReducer.menu,
+    total: state.BurgerReducer.total,
   };
 };
 
-export default connect(mapStateToProps, null)(BaiTapHamberger);
+export default connect(mapStateToProps, mapDispatchToProps)(BaiTapHamberger);
--- src/redux/BurgerReducer.jsx
+++ src/redux/BurgerReducer.jsx
@@ -1,10 +1,25 @@
 const burgerState={
-    burger: {salad:3 ,cheese: 2, beef: 1}, //[{name: "salad", amount: 1}, {name:"cheese", amount: 1},{name: "beef", amount: 1}]
+    burger: {salad:1,cheese: 1, beef: 1}, //[{name: "salad", amount: 1}, {name:"cheese", amount: 1},{name: "beef", amount: 1}]
     menu: {salad: 10, cheese: 20, beef: 55},
     total: 85
 }
 
 export const BurgerReducer =(state= burgerState,action) => {
     console.log("redux",action);
+    switch (action.type) {
+        case 'ADD_BREAD_MID':
+            let {propsBurger, amount} = action;
+            let burgerUpDate = {...state.burger};
+            if(amount===-1 && state.burger[propsBurger]<1) {
+                return {...state}
+            }
+            burgerUpDate[propsBurger] +=amount;
+            state.burger=burgerUpDate;
+            state.total +=amount*state.menu[propsBurger];
+            break;
+    
+        default:
+            break;
+    }
     return {...state}
 }
